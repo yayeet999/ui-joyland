f4e5bdcfe88304c5cfb952957e871f43883b6c34
yayeet999 <serenityai904@gmail.com>
2025-03-01 06:01:14 -0600

    fix: remove deleted component imports from index.ts

--- src/data/components/index.ts
+++ src/data/components/index.ts
@@ -1,27 +1,18 @@
-
-import { accordions } from './accordions';
 import { buttons } from './buttons';
 import { charts } from './charts';
-import { footers } from './footers';
 import { forms } from './forms';
 import { galleries } from './galleries';
 import { modals } from './modals';
-import { navigation } from './navigation';
 import { pricing } from './pricing';
-import { sliders } from './sliders';
 
 // Combine all component data into a single object for easy access
 export const componentData = {
-  accordions,
   buttons,
   charts,
-  footers,
   forms,
   galleries,
   modals,
-  navigation,
-  pricing,
-  sliders
+  pricing
 };
 
 export type ComponentCategory = keyof typeof componentData;
